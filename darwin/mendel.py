1c4d667301215a231bcbb77cfa9749842470b629
Christopher Lee <leec@chem.ucla.edu>
2009-10-26 19:35:48 -0700

    more mendel.py cleanup

--- darwin/mendel.py
+++ darwin/mendel.py
@@ -5,12 +5,15 @@ import random
 
 
 class Allele(object):
+    '''A + B --> phenotype of this allele combination'''
     def __init__(self, name, phenotype):
         self.name = name
         self.phenotype = phenotype
+
     def rvs(self, n):
         'get phenotype sample of size n'
         return (self.phenotype.rvs(n))
+
     def __repr__(self):
         return self.name
 
@@ -26,8 +29,15 @@ class Chromosome(list):
     '''list of (pos,allele) tuples, where x is position expressed as a
     genetic distance in Morgans, e.g.
     chr1 = Chromosome([(0.01,Wh),(0.3,Wr)])'''
-    def __mul__(self,other):
-        'return recombinants of self and other'
+    def __add__(self, other):
+        'return list of phenotypes of self+other'
+        l = []
+        for i,(pos,allele) in enumerate(self):
+            l.append(allele + other[i][1])
+        return l
+    
+    def __mul__(self, other):
+        'return homologous recombination products of self and other'
         swap = 0
         recomb = (Chromosome([self[0]]),Chromosome([other[0]]))
         for i in range(1,len(self)):
@@ -38,21 +48,22 @@ class Chromosome(list):
         return recomb
 
 
-class Genome(dict):
+class DiploidGenome(dict):
     '''dictionary of chromosomes whose keys are chromosome IDs,
     and whose associated values are tuples each containing
     a pair of Chromosome instances.
     e.g.
-    g = Genome({1:(chr1a,chr1b), 2:(chr2a,chr2b)})'''
-    def __mul__(self,other):
+    g = DiploidGenome({1:(chr1a,chr1b), 2:(chr2a,chr2b)})'''
+    def __mul__(self, other):
         'return single progeny of self and other'
-        child = Genome()
+        child = self.__class__()
         for k,chrPair in self.items():
             chrSelf = (chrPair[0]*chrPair[1])[random.randint(0,1)]
             otherPair = other[k]
             chrOther = (otherPair[0]*otherPair[1])[random.randint(0,1)]
             child[k] = (chrSelf,chrOther)
         return child
+
     def __repr__(self):
         s = '{'
         afterFirst = False
@@ -67,11 +78,11 @@ class Genome(dict):
                 s += '(%s,%s)' % (repr(chrPair[0][i][1]),repr(chrPair[1][i][1]))
             s += ']'
         return s+'}'
-    def __call__(self):
+
+    def get_phenotypes(self):
         'get phenotype list'
         l = []
         for k,chrPair in self.items():
-            for i in range(len(chrPair[0])):
-                l.append(chrPair[0][i][1] + chrPair[1][i][1])
+            l += chrPair[0] + chrPair[1]
         return l
 
