5d579f0c0cf907e885800003f8cbfed1546f274f
Christopher Lee <leec@chem.ucla.edu>
2010-05-24 15:14:54 -0700

    removed redundant parts of robomendel_cross_obs()

--- test/robomendel_test.py
+++ test/robomendel_test.py
@@ -364,9 +364,10 @@ def subgraph_pl_test(modelDict=dict(mix=mixture_model,
     llDict = model.posterior_ll(fmerge)
     print_pl(llDict)
    
-def robomendel_cross_test(modelDict=dict(mix=mixture_model,
+def robomendel_cross_obs(modelDict=dict(mix=mixture_model,
                                     family=family_model,
-                                    unrelated=unrelated_model)):
+                                    unrelated=unrelated_model,
+                                    environmental=environmental_model)):
     from darwin.robomendel import PeaPlant
     purple_plant = PeaPlant(genome=PeaPlant.purple_genome)
     white_plant = PeaPlant(genome=PeaPlant.white_genome)
@@ -387,35 +388,6 @@ def robomendel_cross_test(modelDict=dict(mix=mixture_model,
     #obsSet.add_obs(purple_plant.rvs(n), var='dad', matingID=i)
     #obsSet.add_obs(white_plant.rvs(n), var='child', matingID=i)
 
-
-    p = 1./len(modelDict) # uninformative prior
-    modelWh = stats.norm(0, 1)
-    modelPu = stats.norm(10, 1)
-    stop = model.StopState(useObsLabel=False)
-    d = {}
-    d2 = {}
-    for model_name, model_f in modelDict.items(): # build distinct models
-        state = model.SilentState(model_name)
-        state.subgraph = model_f(modelWh, modelPu)
-        d[state] = p
-        d2[state] = {stop:1.}
-    prior = model.StateGraph({'START':d})
-    term = model.StateGraph(d2)
-    dg = model.DependencyGraph({'START':{'model':prior},
-                                'model':{'STOP':term}})
-    m = model.Model(dg, obsSet)
-    m.segmentGraph.p_forward(m.logPobsDict)
-    f = m.segmentGraph.fprob[m.start].f
-    fmerge = {}
-    for node, logP in f.items(): # merge forward calcs from subgraphs
-        try:
-            subgraph = node.segmentGraph
-        except AttributeError:
-            pass
-        else:
-            merge_forward_dict(subgraph.fprob[subgraph.start].f, logP, fmerge)
-    llDict = model.posterior_ll(fmerge)
-    print_pl(llDict)
 
 
 
